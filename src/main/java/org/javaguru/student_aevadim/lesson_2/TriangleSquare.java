c05cf79324494066787bd057036d13d176bc7869
vadimaev <aevadim@yandex.ru>
2025-05-18 17:22:22 +0300

    Lesson 2 get number from user

---
+++ src/main/java/org/javaguru/student_aevadim/lesson_2/TriangleSquare.java
@@ -0,0 +1,18 @@
+package org.javaguru.student_aevadim.lesson_2;
+
+import java.util.Scanner;
+
+public class TriangleSquare {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.println(number1);
+
+    }
+
+}
