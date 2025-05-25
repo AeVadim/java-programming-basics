f5f1c8c8f84e37cc4bbc5433cf4f185d2b368aa7
vadimaev <aevadim@yandex.ru>
2025-05-25 10:09:40 +0300

    Lesson 3 BancAccount

---
+++ src/main/java/org/javaguru/student_aevadim/lesson_3/BancAccount.java
@@ -0,0 +1,16 @@
+package org.javaguru.student_aevadim.lesson_3;
+
+class BancAccount {
+
+    no usages
+    private String owner;
+    no usages
+    private int money;
+
+public BancAccount(String owner, int money)
+    this.owner = owner;
+    this.money = money;
+
+
+
+}
---
+++ src/main/java/org/javaguru/student_aevadim/lesson_3/BankAccountApplication.java
@@ -0,0 +1,12 @@
+package org.javaguru.student_aevadim.lesson_3;
+
+class BankAccountApplication {
+
+    public static void main(String[] args){
+        BancAccount bancAccount1 = new BancAccount("Vasja", 1);
+        BancAccount bancAccount2 = new BancAccount("Petja", 2);
+        BancAccount bancAccount3 = new BancAccount("Tanja", 3);
+    }
+
+
+}
